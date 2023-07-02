0fc7991318b70d6b494102d6659baa4c9a540a83
Pheonix14 <97115569+Pheonix14@users.noreply.github.com>
2023-07-02 10:41:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,170 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const qrCode = require('qrcode');
+const config = require('./config.json')
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// Set the upload limit (default: 200MB)
+const uploadLimit = config.uploadLimit || 200 * 1024 * 1024;
+app.use(fileUpload({
+  limits: { fileSize: uploadLimit }
+}));
+
+
+// Serve the login page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// Handle login form submission
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Read the login credentials from config.json
+  fs.readFile(path.join(__dirname, 'config.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    try {
+      const config = JSON.parse(data);
+
+      // Check if the provided credentials match the ones in config.json
+      if (username === config.username && password === config.password) {
+        // Redirect to the upload page if the login is successful
+        return res.redirect('/upload');
+      } else {
+        // Show an error message if the login is unsuccessful
+        return res.status(401).send('Invalid username or password');
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
+// Serve the upload form page
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, 'upload.html'));
+});
+
+// Handle file upload
+app.post('/upload', async (req, res) => {
+  // Check if a file was uploaded
+  if (!req.files || !req.files.file) {
+    return res.status(400).send('No file was uploaded.');
+  }
+
+  const file = req.files.file;
+  const fileExtension = path.extname(file.name);
+  const validExtensions = ['.pdf', '.jpg', '.jpeg', '.png', '.gif', '.mp4', '.mov', '.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx', '.zip'];
+
+  // Check if the file extension is valid
+  if (!validExtensions.includes(fileExtension)) {
+    return res.status(400).send('Invalid file type. Only PDF, images, videos, Microsoft Office files, and ZIP files are allowed.');
+  }
+
+  const fileName = `${uuidv4()}${fileExtension}`;
+  const filePath = path.join(__dirname, 'uploads', fileName);
+  const downloadLink = `${config.domain}/download/${fileName}`;
+
+  // Move the uploaded file to the server
+  file.mv(filePath, async (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error occurred while uploading the file.');
+    }
+
+    // Generate the QR code image
+    const qrCodeImage = await qrCode.toDataURL(downloadLink);
+
+    // Display the file name, download link, and QR code on the upload success page
+    res.send(`
+      <h2>File uploaded successfully!</h2>
+      <p>File name: ${file.name}</p>
+      <p>Download link: <a href="${downloadLink}">${downloadLink}</a></p>
+      <img src="${qrCodeImage}" alt="QR Code">
+
+      <script>
+        // Auto delete the file after 15 minutes
+        setTimeout(() => {
+          fetch('/delete', {
+            method: 'POST',
+            body: JSON.stringify({ filePath }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then(() => {
+            console.log('File deleted successfully');
+          }).catch((error) => {
+            console.error('Error occurred while deleting the file:', error);
+          });
+        }, 15 * 60 * 1000);
+
+        // Auto delete the file after download
+        const downloadLinkElement = document.querySelector('a');
+        downloadLinkElement.addEventListener('click', () => {
+          fetch('/delete', {
+            method: 'POST',
+            body: JSON.stringify({ filePath }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then(() => {
+            console.log('File deleted successfully');
+          }).catch((error) => {
+            console.error('Error occurred while deleting the file:', error);
+          });
+        });
+      </script>
+    `);
+  });
+});
+
+// Handle file download
+app.get('/download/:fileName', (req, res) => {
+  const { fileName } = req.params;
+  const filePath = path.join(__dirname, 'uploads', fileName);
+
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error occurred while downloading the file.');
+    }
+
+    // Delete the file after download
+    deleteFile(filePath);
+  });
+});
+
+// Handle file deletion
+app.post('/delete', (req, res) => {
+  const { filePath } = req.body;
+
+  deleteFile(filePath);
+  res.status(200).send('File deleted successfully.');
+});
+
+// Delete file
+function deleteFile(filePath) {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('File deleted successfully');
+    }
+  });
+}
+
+// Start the server
+app.listen(config.port, () => {
+  console.log(`Server started on port ${config.port}`);
+});
+  
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Phoenix Share
+
+This is a simple file sharing system built using Node.js and Express. It allows users to securely upload and share files with others. Please note that this system does not include encryption for the uploaded files.
+
+## Disclaimer
+
+**IMPORTANT:** This file sharing system does not provide encryption for the uploaded files. It is meant for simple file sharing purposes and should not be used for sensitive or confidential data. The files uploaded to the system are stored on the server in their original format without encryption. Therefore, it is recommended to exercise caution when uploading and sharing files containing sensitive information.
+
+## Features
+
+- User authentication: Users can log in with their credentials to access the file sharing system.
+- File upload: Users can select and upload files from their devices.
+- File download: Uploaded files can be downloaded using unique download links.
+- QR code generation: Quick sharing and scanning of files through QR codes.
+- File deletion: Automatically deletes files from the server after a set period of time or when downloaded.
+
+## Prerequisites
+
+Before running the system, ensure that you have the following installed:
+
+- Node.js
+- npm (Node Package Manager)
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory using the command line.
+3. Run `npm install` to install the required dependencies.
+4. Fill every details in `config.json`
+## Usage
+
+1. Run `node index.js` or `npm start` to start the server.
+2. Open your web browser and navigate to `http://localhost:3000`.
+3. Log in with your credentials.
+4. On the upload page, select the file you want to share and click the "Upload" button.
+5. Once the upload is successful, you will be provided with a download link and a QR code for sharing.
+6. Share the download link or QR code with others to allow them to download the file.
+7. The file will be automatically deleted from the server after a set period of time or when downloaded.
+
+## Customization
+
+- You can customize the upload limit by modifying the `uploadLimit` in `config.json file`.
+- To change the server port, update the port number in `config.json` file.
+- You can customize the  `username` and `password` by modifying the `config.json` file.
+## License
+
+[MIT License](LICENSE)
+
+Feel free to customize and modify this file sharing system according to your needs. Contributions are welcome!
